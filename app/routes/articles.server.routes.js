2ff06f059066e130db15a224f09073e148265c68
adrianescat <adrian.escat@gmail.com>
2016-01-17 16:52:24 -0300

    Fix al commit anterior

--- app/routes/articles.server.routes.js
+++ app/routes/articles.server.routes.js
@@ -1,14 +1,15 @@
-var articles = require('../../app/controllers/articles.server.controller');
+var users = require('../../app/controllers/users.server.controller'),
+    articles = require('../../app/controllers/articles.server.controller');
 
 module.exports = function(app) {
   app.route('/api/articles')
      .get(articles.list)
-     .post(articles.requiresLogin, articles.create);
+     .post(users.requiresLogin, articles.create);
   
   app.route('/api/articles/:articleId')
      .get(articles.read)
-     .put(articles.requiresLogin, articles.hasAuthorization, articles.update)
-     .delete(articles.requiresLogin, articles.hasAuthorization, articles.delete);
+     .put(users.requiresLogin, articles.hasAuthorization, articles.update)
+     .delete(users.requiresLogin, articles.hasAuthorization, articles.delete);
 
   app.param('articleId', articles.articleByID);
 };
